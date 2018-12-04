54798aa4ee1565e3266a0b0455aab071bdcd6c65
Unknown <boczelijah@gmail.com>
2018-12-04 16:21:11 -0500

    add support for netlify deployment

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9c8d212949bd4101efcf";
+/******/ 	var hotCurrentHash = "e853289232674ce69e31";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
