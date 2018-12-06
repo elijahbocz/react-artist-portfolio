827c8496a7533d0c06a85a93a41454eca2e65725
Unknown <boczelijah@gmail.com>
2018-12-05 19:31:15 -0500

    hacky stuff with the modal

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "00b2b1726c4105acaa96";
+/******/ 	var hotCurrentHash = "e83b3272f8c39921da7a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Image.js
+++ components/Image.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import Modal from './Modal';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  padding: 0;
+  margin: 0;
+  border: none;
+
+  img {
+    width: 100%;
+  }
+`;
 
 class Image extends Component {
-  state = { show: false }
+  state = { show: false };
 
   showModal = () => {
     this.setState({ show: true });
-  }
-  
+  };
+
   hideModal = () => {
     this.setState({ show: false });
-  }
-    
+  };
+
   render() {
     return (
       <main>
         <h1>React Modal</h1>
-        <Modal show={this.state.show} handleClose={this.hideModal} >
+        <Modal show={this.state.show} handleClose={this.hideModal}>
           <p>Modal</p>
           <p>Data</p>
         </Modal>
-        <button type='button' onClick={this.showModal}>Open</button>
+        <StyledButton type="button" onClick={this.showModal}>
+          <img src="https://res.cloudinary.com/elijahboatscloud/image/upload/v1543953365/artwork/artist-portfolio/pa7.jpg" />
+        </StyledButton>
       </main>
-    )
+    );
   }
 }
 
