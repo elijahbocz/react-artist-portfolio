eb3b33265204036f0152724935e50273e07cf391
Unknown <boczelijah@gmail.com>
2018-12-06 20:23:26 -0500

    add meta stuff for mobile and favicon

--- components/Meta.js
+++ components/Meta.js
@@ -4,9 +4,11 @@ const Meta = () => (
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
+    <meta name="theme-color" content="#193441" />
     {/* <link rel="shortcut icon" href="/static/favicon.png" /> */}
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <link rel="stylesheet" href="https://d1azc1qln24ryf.cloudfront.net/114779/Socicon/style-cf.css?9ukd8d" />
+    <link rel="shortcut icon" href="./images/favicon.png" / >
     <title>Artist Name</title>
   </Head>
 );
